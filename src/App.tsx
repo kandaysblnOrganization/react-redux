5f59fc2b1daf390c467d22518c7d35bc24cd5857
popensisa <islamzhenishov.work@gmail.com>
2022-08-04 23:35:50 +0500

    work

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import Header from "./components/Header/Header";
 import Products from "./components/Products/Products";
 
+
+// hello 
 const App = () => {
     return (
         <>
